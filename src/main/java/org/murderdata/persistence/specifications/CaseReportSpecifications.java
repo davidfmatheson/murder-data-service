f28fd3248f39fc4419282264e6a1f9306156f68e
David Matheson <david.f.matheson@gmail.com>
2017-03-25 23:29:47 -0400

    Cases filtered by year and agency

--- src/main/java/org/murderdata/persistence/specifications/CaseReportSpecifications.java
+++ src/main/java/org/murderdata/persistence/specifications/CaseReportSpecifications.java
@@ -5,10 +5,22 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.murderdata.persistence.model.Agency;
 import org.murderdata.persistence.model.CaseReport;
 import org.springframework.data.jpa.domain.Specification;
 
 public class CaseReportSpecifications {
+	public static Specification<CaseReport> identity() {
+		return new Specification<CaseReport>() {
+
+			@Override
+			public Predicate toPredicate(Root<CaseReport> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+				return builder.and();
+			}
+			
+		};
+	}
+
 	public static Specification<CaseReport> yearIs(Integer year) {
 		return new Specification<CaseReport>() {
 
@@ -19,4 +31,16 @@ public class CaseReportSpecifications {
 			
 		};
 	}
+
+	public static Specification<CaseReport> agencyIs(Agency agency) {
+		return new Specification<CaseReport>() {
+
+			@Override
+			public Predicate toPredicate(Root<CaseReport> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+				return builder.equal(root.get("agency"), agency);
+			}
+			
+		};
+	}
+
 }
--- src/main/java/org/murderdata/service/CaseReportController.java
+++ src/main/java/org/murderdata/service/CaseReportController.java
@@ -1,17 +1,22 @@
 package org.murderdata.service;
 
+import static org.springframework.data.jpa.domain.Specifications.where;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import javax.annotation.Resource;
 
+import org.murderdata.persistence.model.Agency;
 import org.murderdata.persistence.model.CaseReport;
 import org.murderdata.persistence.repositories.CaseReportRepository;
 import org.murderdata.persistence.specifications.CaseReportSpecifications;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +26,23 @@ public class CaseReportController {
 	private CaseReportRepository caseReportRepository;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<CaseReport> getAll() {
-		Iterable<CaseReport> modelsIterable = this.caseReportRepository.findAll(CaseReportSpecifications.yearIs(1978));
+	public List<CaseReport> getAll(
+			@RequestParam(name = "year", required = false) Integer year,
+			@RequestParam(name = "agencyId", required = false) Long agencyId
+			) {
+		Specification<CaseReport> specification = where(CaseReportSpecifications.identity());
+		
+		if (year != null) {
+			specification = where(specification).and(CaseReportSpecifications.yearIs(year));
+		}
+		
+		if (agencyId != null) {
+			Agency agency = new Agency();
+			agency.setId(agencyId.longValue());
+			specification = where(specification).and(CaseReportSpecifications.agencyIs(agency));
+		}
+		
+		Iterable<CaseReport> modelsIterable = this.caseReportRepository.findAll(specification);
 		List<CaseReport> models = StreamSupport.stream(modelsIterable.spliterator(), false)
 		                .collect(Collectors.toList());	
 		return models;
